7dcf2cede429944e76986e7fbe812fee20d9386b
grec88 <Lihtinmann88@yandex.ru>
2024-08-04 15:14:14 +0300

    feat: the towel sort task solved

--- src/index.js
+++ src/index.js
@@ -2,5 +2,19 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let res = '';
+  if(JSON.stringify(matrix) !== JSON.stringify([]) && matrix){
+    for(i = 0; i < matrix.length; i++){
+      if(i % 2 !== 0){
+        matrix[i].reverse();
+      }
+      res += matrix[i].join(',');
+      res += i !== matrix.length - 1 ? ',' : '';
+    }
+    res = res.split(',');
+  }
+  else{
+    res = [];
+  }
+  return res;
 }
